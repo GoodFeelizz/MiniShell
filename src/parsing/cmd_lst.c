62bb6c8a5c07d1546b69375ab904dc695b1dbe4f
Tom Gomez <togomez@1G2.42angouleme.fr>
2025-04-25 11:24:43 +0200

    Minishell 1.1

--- src/parsing/cmd_lst.c
+++ src/parsing/cmd_lst.c
@@ -1,53 +1,59 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   token_lst.c                                        :+:      :+:    :+:   */
+/*   cmd_lst.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: togomez <togomez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/17 10:38:25 by togomez           #+#    #+#             */
-/*   Updated: 2025/04/25 11:21:05 by togomez          ###   ########.fr       */
+/*   Created: 2025/04/25 08:14:46 by togomez           #+#    #+#             */
+/*   Updated: 2025/04/25 11:22:08 by togomez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-t_token	*token_new(char *val, t_arg type)
+t_cmd	*cmd_new(void)
 {
-	t_token	*new;
+	t_cmd	*new;
 
-	new = ft_calloc(sizeof(t_token), 1);
+	new = ft_calloc(sizeof(t_cmd), 1);
 	if (!new)
 		return (NULL);
-	new->val = val;
-	new->type = type;
+	new->arguments = NULL;
+	new->redirection = NULL;
+	new->exit_s = 0;
+	new->intput_fd = -1;
+	new->output_fd = -1;
 	return (new);
 }
 
-t_token	*token_last(t_token *lst)
+t_cmd	*cmd_last(t_cmd *lst)
 {
 	while (lst && lst->next)
 		lst = lst->next;
 	return (lst);
 }
 
-void	token_add_back(t_token **lst, t_token *new)
+void	cmd_add_back(t_cmd **lst, t_cmd *new)
 {
 	if (!lst || !new)
 		return ;
 	if (*lst)
-		token_last(*lst)->next = new;
+		cmd_last(*lst)->next = new;
 	else
 		*lst = new;
 }
 
-void	token_clear(t_token *token)
+void	cmd_clear(t_cmd **cmd)
 {
-	if (token && token->next)
-		token_clear(token->next);
-	if (token)
-	{
-		free(token->val);
-		free(token);
-	}
+	if (!cmd || !*cmd)
+		return ;
+	if ((*cmd)->next)
+		cmd_clear(&(*cmd)->next);
+	if ((*cmd)->arguments)
+		lstclear(&(*cmd)->arguments);
+	if ((*cmd)->redirection)
+		token_clear(&(*cmd)->redirection);
+	free(*cmd);
+	*cmd = NULL;
 }
